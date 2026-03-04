22bedc2537cba18cfa95c5409f96080c9334701e
Niels Richard Hansen <Niels.R.Hansen@math.ku.dk>
2026-03-04 17:54:24 +0100

    Theoretical moments for beta and gamma dist.

--- src/distributions.jl
+++ src/distributions.jl
@@ -8,3 +8,13 @@ function rand(x::signed_gamma)
     sign = rand(Bernoulli(x.p)) ? 1 : -1
     return sign * rand(Gamma(x.α, x.θ))
 end
+
+function raw_moments(x::Sampleable, k::Int)
+    if x isa Beta
+        return prod((x.α + i) / (x.α + x.β + i) for i in 0:k-1)
+    elseif x isa Gamma
+        return prod((x.α + i) * x.θ for i in 0:k-1)
+    else
+        error("raw_moments not implemented for type $(typeof(x))")
+    end
+end
