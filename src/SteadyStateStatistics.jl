883981b38d74aa063fd9fa3fdb507285561b2494
Niels Richard Hansen <Niels.R.Hansen@math.ku.dk>
2026-03-04 17:58:12 +0100

    Extending the cumulants function to compute theoretical cumulants

--- src/SteadyStateStatistics.jl
+++ src/SteadyStateStatistics.jl
@@ -5,16 +5,16 @@ using Distributions # For Exponential, Gamma, Beta
 using StatsBase # For Weights and sample
 using Plots
 using Tullio # For efficient tensor operations
-using Cumulants # For efficient cumulant estimation
 using ExponentialUtilities # For matrix exponentials 
 import Base: rand
+import Cumulants: cumulants # For efficient cumulant estimation
 
 include("jump_process.jl")
 include("distributions.jl")
 include("osd.jl")
 include("estimation.jl")
 
-export jump_process, compound_poisson, compound_gamma, compound_signed_gamma, compound_beta, linear_estimator, quadratic_estimator, two_step_estimator, quadratic_estimator_4, two_step_shrinkage_estimator
+export jump_process, compound_poisson, compound_gamma, compound_signed_gamma, compound_beta, cumulants, linear_estimator, quadratic_estimator, two_step_estimator, quadratic_estimator_4, two_step_shrinkage_estimator
 export osd, rand, plot
 
 end # module SteadyStateStatistics
